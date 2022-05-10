9065d6b58d067e3ee58aea782f259067e7b2f330
Nadia Yanina Tolaba <103613141+nadiatolaba@users.noreply.github.com>
2022-05-09 21:42:36 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+for (let i: number = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(i + " es multiplo de 2");
+  } else if (i % 3 === 0) {
+    console.log(i + " es multiplo de 3");
+  }
+}
